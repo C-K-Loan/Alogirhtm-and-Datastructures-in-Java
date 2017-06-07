d9be17640f4af53e7095edb34e19a34978c5ccfa
C-K-Loan <christian.kasim.loan@gmail.com>
2017-06-08 00:36:56 +0200

    Add files via upload

---
+++ RGB pixel detection- image rotation- geometrical figures-inheritance/Cube.java
@@ -0,0 +1,27 @@
+/**
+ * Represents a Cube
+ * @author AlgoDat
+ */
+public class Cube {
+
+	/*
+	 * Constructor without parameter
+	 */
+	public Cube() {
+		//TODO
+	}
+	/*
+	 * Constructor with one parameter
+	 * 
+	 * @param length the side length of the cube
+	 */
+	public Cube(double length) {
+		// TODO
+	}
+
+	public double getLength(){
+		return this.length;
+	}
+	//TODO: ggf. weitere Methoden und member implementieren
+}
+
---
+++ RGB pixel detection- image rotation- geometrical figures-inheritance/Cuboid.java
@@ -0,0 +1,42 @@
+/**
+ * Represents a Cuboid
+ * http://en.wikipedia.org/wiki/Cuboid
+ * 
+ * @author AlgoDat
+ *
+ */
+public class Cuboid{
+
+	double height, length, width;
+	
+	/*
+	 * Constructor for a Cuboid object
+	 */
+	public Cuboid() {
+		//TODO
+	}
+	/*
+	 * Constructor for a Cuboid object
+	 * 
+	 * @param h height
+	 * @param l length
+	 * @param w width
+	 */
+	public Cuboid(double h, double l, double w) {
+		//TODO
+	}
+
+	public double getHeight(){
+		return this.height;
+	}
+	public double getLength(){
+		return this.length;
+	}
+	public double getWidth(){
+		return this.width;
+	}
+	//TODO: ggf. weitere Methoden und member implementieren
+
+
+}
+
